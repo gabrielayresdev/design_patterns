24c0cad94e7a78a06ac177636d0af11531f19e01
gabrielayresdev <gabriel.ar.fort@gmail.com>
2026-04-02 09:31:42 -0300

    feat: add error handling for Zod validation and implement user registration e2e tests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import fastify from "fastify";
 import { userRoutes } from "./http/controllers/users/routes";
+import z, { ZodError } from "zod";
 
 export const app = fastify();
 
 app.register(userRoutes);
+
+app.setErrorHandler((error: unknown, _, reply) => {
+  if (error instanceof ZodError) {
+    reply.status(400).send({
+      error: z.treeifyError(error),
+    });
+  } else {
+    console.log(error);
+  }
+});
