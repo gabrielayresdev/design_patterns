c247c51ee2995034e2f6ccdf644978ceae7a674d
gabrielayresdev <gabriel.ar.fort@gmail.com>
2026-05-09 02:56:50 -0300

    chore: remove user feature and update dependencies

--- src/http/controllers/users/routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { register } from "./register";
-
-export async function userRoutes(app: FastifyInstance) {
-  app.post("/user", register);
-}
--- src/repositories/types/users-repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { Prisma, User } from "@/generated/prisma/client";
-
-export interface UsersRepository {
-  create: (user: Prisma.UserCreateInput) => Promise<User>;
-  findByEmail: (email: string) => Promise<User | null>;
-}
